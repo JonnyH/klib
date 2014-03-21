bbdb427827b944cdf0a016269bcb2bd69e081331
Jonathan Hamilton <jtrhamilton@gmail.com>
2014-03-20 17:36:18 -0700

    gcc -O2 -flto seems to optimise the hash tests out completely

--- tests/hash.c
+++ tests/hash.c
@@ -4,21 +4,35 @@
 
 void t_genfunction(void)
 {
+	volatile uint8_t readBuffer[8];
 	uint8_t buffer[8];
+	for (int i = 0; i < 8; i++)
+		buffer[i] = readBuffer[i];
 	/* Relies on gcc nested functions */
 	KHT_DEFINE_HASH_FN(fn1, sizeof(buffer));
 	KHT_DEFINE_HASH_FN(fn2, 8);
-	TEST_FAIL_IF(fn1(buffer) != fn2(buffer));
+	uint32_t h1 = fn1(buffer);
+	for (int i = 0; i < 8; i++)
+		buffer[i] = readBuffer[i];
+	uint32_t h2 = fn2(buffer);
+	TEST_FAIL_IF(h1 != h2);
 }
 TEST_FN(t_genfunction);
 
 void t_genfunctionsimple(void)
 {
+	volatile uint8_t readBuffer[8];
 	uint8_t buffer[8];
+	for (int i = 0; i < 8; i++)
+		buffer[i] = readBuffer[i];
 	/* Relies on gcc nested functions */
 	KHT_DEFINE_SIMPLE_HASH_FN(fn1, sizeof(buffer));
 	KHT_DEFINE_SIMPLE_HASH_FN(fn2, 8);
-	TEST_FAIL_IF(fn1(buffer) != fn2(buffer));
+	uint32_t h1 = fn1(buffer);
+	for (int i = 0; i < 8; i++)
+		buffer[i] = readBuffer[i];
+	uint32_t h2 = fn2(buffer);
+	TEST_FAIL_IF(h1 != h2);
 }
 TEST_FN(t_genfunctionsimple);
 
